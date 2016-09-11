82d83fb1b8b81d63d82d805cde6a94e9736a1af2
upshkin <upshkin@gmail.com>
2016-09-11 11:39:28 +0300

    Первая версия handleRequest

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
-// Массив доступных страниц
-$pages = array('home', 'test');
-// Определяем страницу для вывода
-$page = '';
-// Если запрос не пуст - проверяем, есть ли он в массиве наших страниц
-if (!empty($_REQUEST['q'])) {
-$request = explode('/', $_REQUEST['q']);
-// Если есть - окей, всё верно, используем это имя
-if (in_array(strtolower($request[0]), $pages)) {
-$page = strtolower($request[0]);
-}
-}
-// Иначе используем страницу по умолчанию
-if (empty($page)) {
-$page = 'home';
-}
+ini_set('display_errors', 1);
+ini_set('error_reporting', -1);
 
-echo "Мы выводим страницу <b>{$page}<b>";
+if (!class_exists('Core')) {
+    require_once 'core/Core.php';
+}
+$Core = new Core();
+
+$req = !empty($_REQUEST['q'])
+    ? trim($_REQUEST['q'])
+    : '';
+$Core->handleRequest($req);
